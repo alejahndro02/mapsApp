7a496202e4e2dc8744c8a3467b87dab4d94d7028
alejahndro02 <alejahndro02@gmail.com>
2022-04-16 18:08:15 -0500

    se exporto el componente de mapScreen para poder ser usado en appcomponent

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule           } from '@angular/core';
+import { CommonModule       } from '@angular/common';
+import { MapScreenComponent } from './screens/map-screen/map-screen.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MapScreenComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    MapScreenComponent
+
   ]
 })
 export class MapsModule { }
