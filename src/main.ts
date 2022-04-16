b9840494d63dc9f92439dd8969dc48ef151d3517
alejahndro02 <alejahndro02@gmail.com>
2022-04-16 18:12:44 -0500

    se crea una condicion que evalua si el navedor sopora la geolocalización

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode         } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule              } from './app/app.module';
+import { environment            } from './environments/environment';
+
+if (!navigator.geolocation){
+  alert('El navegador no soportta la eolocalización')
+  throw new Error('El navegador no soportta la eolocalización')
+}
+
 
 if (environment.production) {
   enableProdMode();
