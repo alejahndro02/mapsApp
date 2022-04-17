08095e0213c74757279e0f92b46e00df97f39046
alejahndro02 <alejahndro02@gmail.com>
2022-04-17 13:35:58 -0500

    se agregraron confirguraciones extras ara el funcionamiento de mapbox

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule              } from './app/app.module';
 import { environment            } from './environments/environment';
 
+import   Mapboxgl                 from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamFobmRybyIsImEiOiJjbDB1Ymw0eTIwN3JkM2ttdGp5dXI5M29rIn0.dCeUmYGiS_dgDnStR_D0-w';
+
 if (!navigator.geolocation){
   alert('El navegador no soportta la eolocalización')
   throw new Error('El navegador no soportta la eolocalización')
