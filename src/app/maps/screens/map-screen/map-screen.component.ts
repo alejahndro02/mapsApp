5bb74566f22d26d1f17f098f51434479c82c62b3
alejahndro02 <alejahndro02@gmail.com>
2022-04-17 09:58:30 -0500

    Se inyecta el servicio de lugaresService y se crea un getter que retorna lo que viene en isUserLocationReady proveniente del servicio

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { LugaresService    } from '../../services/lugares.service';
+import { Component      } from '@angular/core';
+import { LugaresService } from '../../services/lugares.service';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
   styleUrls: ['./map-screen.component.css']
 })
-export class MapScreenComponent implements OnInit {
+export class MapScreenComponent {
 
   constructor(private lugaresService:LugaresService) { }
 
-  ngOnInit(): void {
+  get isUserLocationReady(){
+    return this.lugaresService.isUserLocationReady;
   }
-
 }
