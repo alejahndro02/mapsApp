84e31bee7232c2c1f40e4a80b40eef141516f9ab
alejahndro02 <alejahndro02@gmail.com>
2022-04-23 13:11:24 -0500

    Se decalro el metodo onqueryChanged que se dispara despues de un segundo sin escuchar ninguna instruccion

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  // contorolar el comportamirnto de debounce
+  private debounceTimer?:NodeJS.Timeout;
 
   constructor() { }
 
-  ngOnInit(): void {
+  onQueryChanged(query:string=''){
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+    this.debounceTimer= setTimeout(()=>{
+      console.log("mandar este cuery", query);
+    }, 1000)
   }
-
 }
