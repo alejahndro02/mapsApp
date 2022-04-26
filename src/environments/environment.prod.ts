876d0cd6ccba35a968ea26ff806b7d54fded2cbe
alejahndro02 <alejahndro02@gmail.com>
2022-04-26 00:00:56 -0500

    se agregaron los environmets de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
+  mapToken:'pk.eyJ1IjoiYWxlamFobmRybyIsImEiOiJjbDB1Ymw0eTIwN3JkM2ttdGp5dXI5M29rIn0.dCeUmYGiS_dgDnStR_D0-w',
 };
