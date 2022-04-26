9a09d1ffc706f2797122d0ae07dae776a7d3c62b
alejahndro02 <alejahndro02@gmail.com>
2022-04-25 22:37:37 -0500

    se importo el httpClient, los environment lo features y los placesResponse, se inyecto el servicio de httpClient y se creo el metodo de getPlacesByQuery

--- src/app/maps/services/lugares.service.ts
+++ src/app/maps/services/lugares.service.ts
@@ -1,30 +1,50 @@
-import { Injectable } from '@angular/core';
+import { HttpClient     } from '@angular/common/http';
+import { Injectable     } from '@angular/core';
+import { environment    } from '../../../environments/environment'
+import { Feature, 
+         PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugaresService {
-  useLocations?: [number,number];
-get isUserLocationReady():boolean{
-  return !!this.useLocations;
-}
-  constructor() {
-    this.getUserLocation();
+  useLocation?: [number,number];
+  isLoadingPlaces:boolean = false;
+  places:Feature[]=[];
+
+  get isUserLocationReady():boolean{
+    return !!this.useLocation;
+  }
+
+  constructor( 
+    private http: HttpClient ) {
+      this.getUserLocation();
    }
 
-  getUserLocation():Promise<[number,number]>{
+  async getUserLocation():Promise<[number,number]>{
     return new Promise((resolve, reject)=>{
       navigator.geolocation.getCurrentPosition(
         ({coords}) => {
-          this.useLocations= [coords.longitude, coords.latitude]
-          resolve(this.useLocations)
+          this.useLocation = [coords.longitude, coords.latitude];
+          resolve(this.useLocation)
         },
         (err)=>{
-          alert('No se puedo obeteer la geolocalizacin')
+          alert('No se puedo obetener la Geolocalización')
           console.log(err);
           reject();
         }
       )
     })
+  }
+  getPlacesByQuery(query:string=""){
+    // Evaluar cuando el query es nulo 
+    this.isLoadingPlaces= true
+    // this.http.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/{query}.json?proximity=-99.08444586145016%2C19.394061899610065&types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoiYWxlamFobmRybyIsImEiOiJjbDB1Ymw0eTIwN3JkM2ttdGp5dXI5M29rIn0.dCeUmYGiS_dgDnStR_D0-w`)
+    this.http.get<PlacesResponse>(`${environment.mapUrl}/${query}.json?proximity=${this.useLocation}&types=place%2Cpostcode%2Caddress&access_token=${environment.mapToken}`)
+    .subscribe(resp =>{
+      console.log(resp.features);
+      this.isLoadingPlaces = false
+      this.places = resp.features
+    });
   }
 }
