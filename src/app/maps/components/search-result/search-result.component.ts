d6dbdda6070ac22aba4953069bad8396cc6906f8
alejahndro02 <alejahndro02@gmail.com>
2022-05-01 09:54:15 -0500

    se importaron los features de las interfaces y el lugaresService, tambien se creo un getter isLoadingPlaces y places

--- src/app/maps/components/search-result/search-result.component.ts
+++ src/app/maps/components/search-result/search-result.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, 
+         OnInit         } from '@angular/core';
+import { Feature        } from '../../interfaces/places';
+import { LugaresService } from '../../services/lugares.service';
 
 @Component({
   selector: 'app-search-result',
@@ -7,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private lugaresService: LugaresService) { }
+  get isLoadingPlaces():boolean{
+    return this.lugaresService.isLoadingPlaces
+  }
+  get places():Feature[]{
+    return this.lugaresService.places
+  }
 
   ngOnInit(): void {
   }
